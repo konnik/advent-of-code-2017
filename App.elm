78ba3d2d93c903b7763b0d34f955b903965fc84e
konnik <niklas.konstenius@gmail.com>
2017-12-04 06:49:01 +0100

    Solution for day 4 of 2017

--- App.elm
+++ App.elm
@@ -14,6 +14,7 @@ import Puzzles.Year2015.Day7
 import Puzzles.Year2017.Day1
 import Puzzles.Year2017.Day2
 import Puzzles.Year2017.Day3
+import Puzzles.Year2017.Day4
 
 allPuzzles : List Puzzle
 allPuzzles = [ Puzzles.Year2015.Day1.puzzle
@@ -23,6 +24,7 @@ allPuzzles = [ Puzzles.Year2015.Day1.puzzle
              , Puzzles.Year2017.Day1.puzzle 
              , Puzzles.Year2017.Day2.puzzle 
              , Puzzles.Year2017.Day3.puzzle 
+             , Puzzles.Year2017.Day4.puzzle 
              ]
 
 main : Program Never Model Msg
---
+++ Puzzles/Year2017/Day4.elm
@@ -0,0 +1,47 @@
+module Puzzles.Year2017.Day4 exposing (..)
+
+import AdventOfCode.Puzzle exposing (Puzzle, PuzzleSolver, TestSuite, TestResult)
+
+import Set exposing (Set)
+
+puzzle : Puzzle
+puzzle = (2017,4,"High-Entropy Passphrases", tests, part1, part2)
+
+tests : TestSuite
+tests = [ (part1 "aa bb cc dd ee" == "1",  "Part 1 - a")
+        , (part1 "aa bb cc dd aa" == "0",  "Part 1 - b")
+        , (part1 "aa bb cc dd aaa" == "1",  "Part 1 - c")
+        , (part2 "abcde fghij" == "1",  "Part 2 - a")
+        , (part2 "abcde xyz ecdab" == "0",  "Part 2 - b")
+        , (part2 "a ab abc abd abf abj" == "1",  "Part 2 - c")
+        , (part2 "iiii oiii ooii oooi oooo" == "1",  "Part 2 - d")
+        , (part2 "oiii ioii iioi iiio" == "0",  "Part 2 - e")
+        ]
+
+part1 : PuzzleSolver
+part1 input = 
+        String.lines input
+        |> List.map String.words
+        |> List.filter withoutDuplicates
+        |> List.length
+        |> toString
+
+withoutDuplicates : List String -> Bool
+withoutDuplicates list =
+        Set.size (Set.fromList list) == (List.length list)
+
+part2 : PuzzleSolver
+part2 input =
+        String.lines input
+        |> List.map String.words
+        |> List.filter withoutAnagrams 
+        |> List.length
+        |> toString
+
+withoutAnagrams : List String -> Bool
+withoutAnagrams line = 
+        line
+        |> List.map (String.split "") 
+        |> List.map List.sort
+        |> List.map String.concat
+        |> withoutDuplicates
