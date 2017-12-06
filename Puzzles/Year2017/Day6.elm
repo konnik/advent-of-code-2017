b64ea3afa36b10f4b4e9a3706dc5fbb841528168
konnik <niklas.konstenius@gmail.com>
2017-12-06 23:02:46 +0100

    Refactor of day 6

--- Puzzles/Year2017/Day6.elm
+++ Puzzles/Year2017/Day6.elm
@@ -2,96 +2,124 @@ module Puzzles.Year2017.Day6 exposing (..)
 
 import AdventOfCode.Puzzle exposing (Puzzle, PuzzleSolver, TestSuite, TestResult)
 
-import List.Extra exposing (..)
-
+import List.Extra as LE
 
 puzzle : Puzzle
-puzzle = ( 2017, 6, "CHANGE ME", tests, part1, part2 )
+puzzle = ( 2017, 6, "Memory Reallocation", tests, part1, part2 )
 
 tests : TestSuite
 tests = 
-    [ ( part1 "test-input" == "expected-output",  "Test part 1" )
-    , ( part2 "test-input" == "expected-output",  "Test part 2" )
+    [ ( part1 "0 2 7 0" == "5",  "Test part 1" )
+    , ( part2 "0 2 7 0" == "4",  "Test part 2" )
     ]
 
-
 type alias Banks = List Int
+type alias State = 
+    { banks: Banks
+    , history: List Banks
+    , loopLength: Int
+    }
 
 part1 : PuzzleSolver
 part1 input = 
-    input
-        |> parseInput
-        |> run []
+    parseInput input
+        |> runUntil infiniteLoop
+        |> .history
         |> List.length
-        |> (+) 1
         |> toString
 
 part2 : PuzzleSolver
-part2 input = 
-    "not implemented"
+part2 input =
+    parseInput input
+        |> runUntil infiniteLoop
+        |> .loopLength
+        |> toString
 
-distribute : Int-> Banks -> Banks
-distribute num banks =
-    case num of
-        0 ->
-            banks
-        n ->
-        case banks of
-            b::bs ->
-                right 1 (distribute (n-1) (left 1 ((b+1)::bs)))
-            _ -> banks
+parseInput : String -> State
+parseInput input =
+    { banks = input |> String.words |> List.map toInt
+    , history = []
+    , loopLength = 0
+    }
 
-left : Int -> List a -> List a
-left n list = 
-    (List.drop n list) ++ (List.take n list)
+infiniteLoop : State -> Bool
+infiniteLoop state = List.member state.banks state.history
 
-right : Int -> List a -> List a
-right n list = 
-     (List.drop ((List.length list )-n) list) ++ (List.take  ((List.length list )-n) list)
-
-
-runIt : Banks -> List Banks
-runIt banks = run [banks] banks
-
-run : List Banks -> Banks -> List Banks
-run list banks = 
+runUntil : (State -> Bool) -> State -> State
+runUntil done state = 
         let
-            banks2 = (distributeNext banks)
+            newBanks = reallocateBlocks state.banks
+            newState = { state | banks = newBanks, history = state.banks :: state.history }
         in
-            if List.member banks2 list then
-                let a = Debug.log "Index:" (elemIndex banks2 list) in
-                list
+            case done newState of
+                True -> { newState 
+                        | loopLength = loopLength newState }
+                False -> runUntil done newState
 
-            else
-                run (banks2::list) banks2
+loopLength : State -> Int
+loopLength state = 
+    1 + Maybe.withDefault 0 (LE.elemIndex state.banks state.history)
 
-distributeNext : Banks -> Banks
-distributeNext banks = 
+reallocateBlocks : Banks -> Banks
+reallocateBlocks banks = 
     let
-        index = nextBankToDistribute banks
-        value = Maybe.withDefault 0 (getAt index banks)
-        bank2 = Maybe.withDefault [] (setAt index 0 banks)
-        rot = ((index +1) % 16)
-        bank3 = left rot bank2
+        bank = mostBlocks banks
+        blocks = blocksOfBank bank banks
     in
-        right rot (distribute value bank3)
+        banks
+            |> zeroBank bank
+            |> rotateLeft (bank + 1)
+            |> distributeBlocks blocks
+            |> rotateRight (bank + 1)
         
-        
+zeroBank : Int -> Banks -> Banks
+zeroBank bank banks = Maybe.withDefault banks (LE.setAt bank 0 banks)
 
-nextBankToDistribute : Banks -> Int
-nextBankToDistribute banks = 
+blocksOfBank : Int-> Banks -> Int
+blocksOfBank bank banks = 
+    Maybe.withDefault 0 (LE.getAt bank banks)
+
+mostBlocks : Banks -> Int
+mostBlocks banks = 
     let 
         max = Maybe.withDefault 0 (List.maximum banks)
     in
-        Maybe.withDefault 0 (elemIndex max banks)
+        Maybe.withDefault 0 (LE.elemIndex max banks)
+
+distributeBlocks : Int -> Banks -> Banks
+distributeBlocks blocks banks =
+    case blocks of
+        0 -> banks
+        _ -> banks 
+            |> insertOne
+            |> rotateLeft 1
+            |> distributeBlocks (blocks-1)
+            |> rotateRight 1
+
+insertOne : Banks -> Banks
+insertOne banks =
+    case banks of
+        [] -> banks
+        x::xs -> (x + 1) :: xs
+
+
+
+--  utillity functions 
+
+toInt : String -> Int
+toInt = Result.withDefault 0 << String.toInt
 
 rotateLeft : Int -> List a -> List a
 rotateLeft n list = 
-    (List.drop n list) ++ (List.take n list)
+    let
+        n2 = n % (List.length list)
+    in 
+        (List.drop n2 list) ++ (List.take n2 list)
 
-parseInput : String -> Banks
-parseInput input =
-    input
-        |> String.words 
-        |> List.map String.toInt
-        |> List.map (Result.withDefault 0)
+rotateRight : Int -> List a -> List a
+rotateRight n list = 
+    let
+        n2 = n % (List.length list)
+    in 
+        (List.drop ((List.length list )-n2) list) ++ (List.take  ((List.length list )-n2) list)
+
