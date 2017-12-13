e8b31e4ac6c60ce54302a99f7c98fd5b150ede7b
konnik <niklas.konstenius@gmail.com>
2017-12-13 18:48:27 +0100

    Refactor part 1 solution

--- Puzzles/Year2017/Day13.elm
+++ Puzzles/Year2017/Day13.elm
@@ -2,155 +2,71 @@ module Puzzles.Year2017.Day13 exposing (..)
 
 import AdventOfCode.Puzzle exposing (Puzzle, PuzzleSolver, TestSuite, TestResult)
 
-import Dict exposing (Dict)
-import List exposing (take, drop, filter, map)
+import List exposing (take, drop, filter, map, sum)
 import String exposing (words, lines)
 
 puzzle : Puzzle
 puzzle = ( 2017, 13, "Packet Scanners", tests, part1, part2 )
 
+testInput : String
 testInput = "0: 3\n1: 2\n4: 4\n6: 4"
 
 tests : TestSuite
 tests = 
-    [
-        ( part1 testInput == "24",  "Test part 1" )
-  , ( part2 testInput == "10",  "Test part 2" )
+    [ ( part1 testInput == "24",  "Test part 1" )
+    , ( part2 testInput == "10",  "Test part 2" )
     ]
 
-type alias Scanner = { depth: Int, range: Int, position: Int,  direction: Int}
-type alias Firewall = Dict Int Scanner
-type alias State = { caught: Bool, position: Int, firewall: Firewall, severity: Int}
+type alias Scanner = (Int, Int)
 
 part1 : PuzzleSolver
 part1 input = 
     parseInput input
-        |> initialState
-        |> stepUntil (pos 100)
-        |> .severity
+        |> tripSeverity
         |> toString
     
 part2 : PuzzleSolver
 part2 input = 
     parseInput input
-        |> Dict.values
-        |> List.map toSimpleScanner
         |> findDelay 0 
         |> toString
 
-scanners : String -> List (Int, Int)
-scanners input = 
-    parseInput input
-        |> Dict.values
-        |> List.map toSimpleScanner
+tripSeverity : List (Int, Int) -> Int
+tripSeverity scanners = 
+    let 
+        severityOf ((depth, range) as scanner) = 
+            case canPassScanner 0 scanner of
+                True -> 0
+                False -> range * depth
+    in
+       sum (map severityOf scanners)
 
 findDelay : Int -> List (Int, Int) -> Int
 findDelay delay scanners = 
-    if canPass delay scanners then
+    if canPassAllScanners delay scanners then
         delay
     else
         findDelay (delay+1) scanners
 
 
-toSimpleScanner : Scanner -> (Int, Int)
-toSimpleScanner {depth, range} = (depth, range)
+canPassAllScanners : Int -> List (Int, Int) -> Bool
+canPassAllScanners delay scanners = 
+    List.all (canPassScanner delay) scanners
 
-canPass : Int -> List (Int, Int) -> Bool
-canPass delay scanners = 
-    List.all (canPassScannerWithDelay delay) scanners
-
-canPassScannerWithDelay : Int -> (Int, Int) -> Bool
-canPassScannerWithDelay delay (depth, range) = 
+canPassScanner : Int -> (Int, Int) -> Bool
+canPassScanner delay (depth, range) = 
     (delay + depth) % ((range-1)*2) /= 0
 
-stepUntil : (State -> Bool) -> State -> State
-stepUntil done state = 
-    let 
-        newState = 
-            move state
-            |> checkCaught
-            |> moveScanners
-    in
-        if done newState then
-            newState
-        else
-            stepUntil done newState
 
-pos : Int ->  State -> Bool
-pos pos state = state.position >= pos
-
-
-moveScanners : State -> State
-moveScanners state =
-    {state | firewall = Dict.map moveScanner state.firewall }
-
-
-moveScanner : Int -> Scanner -> Scanner 
-moveScanner _ s = 
-    let 
-        newPos  = s.position + s.direction
-        newDir = if newPos == 0 then
-                    1
-                else if newPos == (s.range-1) then
-                    -1
-                else
-                    s.direction
-    in 
-        { s | direction = newDir, position  = newPos }
-
-move : State -> State 
-move state = 
-    let 
-        pos = state.position + 1
-    in 
-        { state | position = pos }
-
-checkCaught : State -> State
-checkCaught state = 
-    if caught state then
-        { state | caught = True, severity = state.severity + (severity state)}
-    else
-        state
-
-severity : State -> Int 
-severity state = 
-    case scannerAt state.firewall state.position  of 
-        Nothing -> 0
-        Just scanner -> scanner.range * scanner.depth
-
-
-caught : State -> Bool
-caught state = 
-    case scannerAt state.firewall state.position  of 
-        Nothing -> False
-        Just scanner -> scanner.depth == state.position && scanner.position == 0
-
-
-scannerAt : Firewall -> Int -> Maybe Scanner
-scannerAt firewall pos = Dict.get pos firewall 
-
-initialState : Firewall -> State
-initialState firewall = 
-    { caught = False, position = -1, severity = 0, firewall = firewall}
-
-parseInput : String -> Firewall
+parseInput : String -> List Scanner
 parseInput input = 
-    Dict.fromList (map parseLine (lines input))
+    map parseLine (lines input)
 
--- 2 <-> 0, 3, 4
-parseLine : String  -> (Int, Scanner)
+parseLine : String  -> Scanner
 parseLine line = 
     case words (removeColon line) of
-        [d, r] -> 
-            let 
-                depth = (toInt d)
-                range = (toInt r)
-                scanner = { depth = depth , range = range, position = 0, direction = 1}
-            in
-                (depth, scanner)
-        _ -> 
-            (-100, { depth = -100 , range = 0, position = 0, direction = 0} )
-
+        [depth, range] -> (toInt depth, toInt range)
+        _ -> (999,0)
 
 removeColon : String -> String
 removeColon = String.filter (\ c -> c/= ':')
